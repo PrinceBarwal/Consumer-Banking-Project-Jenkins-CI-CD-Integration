0f011fcf4294b5d5e878a568db92219a8dd9fe50
Prince Saini <princesaini0144@gmail.com>
2024-03-21 14:55:35 +0530

    changes app.java for poll scm build trigger

--- src/main/java/com/consumer/banking/App.java
+++ src/main/java/com/consumer/banking/App.java
@@ -6,8 +6,8 @@ package com.consumer.banking;
  */
 public class App 
 {
-    public static void main( String[] args )
+public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "changes for poll scm build trigger" );
     }
 }

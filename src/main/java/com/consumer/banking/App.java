42593a5dfca5f37049c241d3b73f2c75cae254f0
PrinceBarwal <89983174+PrinceBarwal@users.noreply.github.com>
2024-04-02 23:49:13 +0530

    for ci cd deployment in same job

--- src/main/java/com/consumer/banking/App.java
+++ src/main/java/com/consumer/banking/App.java
@@ -8,6 +8,6 @@ public class App
 {
 public static void main( String[] args )
     {
-        System.out.println( "changes done for github webhook" );
+        System.out.println( "changes done for ci cd deployment in same job" );
     }
 }

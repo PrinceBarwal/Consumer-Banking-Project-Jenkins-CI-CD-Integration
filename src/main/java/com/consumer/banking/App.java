f9204c030c5b6344a07234ab8da18174250f9184
PrinceBarwal <89983174+PrinceBarwal@users.noreply.github.com>
2024-04-02 16:19:39 +0530

    Update App.java

--- src/main/java/com/consumer/banking/App.java
+++ src/main/java/com/consumer/banking/App.java
@@ -8,6 +8,6 @@ public class App
 {
 public static void main( String[] args )
     {
-        System.out.println( "again third time changes for poll scm build trigger" );
+        System.out.println( "again fourth time changes for poll scm build trigger" );
     }
 }
